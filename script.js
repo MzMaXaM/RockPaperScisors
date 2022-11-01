248d855adc0f5ee073e1c45a78f5b11c0c5b2e41
MzMaXaM <55605842+MzMaXaM@users.noreply.github.com>
2022-11-01 19:00:58 +0000

    added reset game logic

--- script.js
+++ script.js
@@ -1,52 +1,69 @@
 const btns = document.querySelectorAll('.player>button')
+const resetBtn = document.querySelector('#resetBtn')
 
 let playerChoice = document.querySelector('#playerChoose')
 let compChoice = document.querySelector('#computerChoose')
 let playerScore = document.querySelector('#playerScore')
 let compScore = document.querySelector('#computerScore')
+
 let cScore = 0
 let pScore = 0
 
 btns.forEach(e => {
-  e.addEventListener("click",(el)=>{
+  e.addEventListener("click", (el) => {
     let pC = el.path[0].id.substring(6)
     let cC = computerChoice()
-
-    playerChoice.textContent = pC
-    compChoice.textContent = cC
     pickWinner(pC, cC)
+    showChoices(pC, cC)
   })
 });
 
+const resetGame = () => {
+  cScore = 0
+  pScore = 0
+  playerScore.textContent = pScore
+  compScore.textContent = cScore
+  showChoices('xxx', 'xxx')
+}
+
+resetBtn.addEventListener('click', resetGame)
 
 const computerChoice = () => {
-  let random = Math.random()*100
-  let objectReturn
-  if (random<33.3) objectReturn = 'Rock'
-  else if (random<66.6) objectReturn = 'Paper'
-  else objectReturn = 'Scisors'
+  let random = Math.random() * 100
+  let choose
+  if (random < 33.3) choose = 'Rock'
+  else if (random < 66.6) choose = 'Paper'
+  else choose = 'Scisors'
+  return choose
+}
 
-  return objectReturn
+const showChoices = (pC, cC) => {
+  playerChoice.textContent = pC
+  compChoice.textContent = cC
 }
 
 const pickWinner = (player, comp) => {
-  if(player==comp) {
-    console.log('Tie!')
+  if (player == comp) {
+    console.log('It\'s a tie!')
     return
   }
   if (player == 'Rock') {
-    if (comp == 'Paper') {
-      console.log('Player lost')
-      cScore ++
-      compScore.textContent = cScore
-    }else{
-      console.log('Player win')
-      pScore ++
-      playerScore.textContent = pScore
-    }
-  }else if (player == 'Paper'){
+    comp == 'Paper' ? increaseScore('comp') : increaseScore('player')
+  } else if (player == 'Paper') {
+    comp == 'Rock' ? increaseScore('player') : increaseScore('comp')
+  } else {
+    comp == 'Rock' ? increaseScore('comp') : increaseScore('player')
+  }
+}
 
-  }else {
-
+const increaseScore = (whoWin) => {
+  if (whoWin == 'player') {
+    console.log('Player win!')
+    pScore++
+    playerScore.textContent = pScore
+  } else {
+    console.log('Player lost')
+    cScore++
+    compScore.textContent = cScore
   }
 }
